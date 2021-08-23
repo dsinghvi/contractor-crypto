090cf576dbdcfb478fb3c930d69d0ac5ddd65a20
Deep Singhvi <dsinghvi@umich.edu>
2021-08-23 15:22:09 -0400

    generate abi for each contract

--- index.js
+++ index.js
@@ -22,4 +22,17 @@ const input = {
 
 const output = JSON.parse(solc.compile(JSON.stringify(input)));
 
-console.log(output); // Log the result
+console.log("Logging contract output")
+console.log(output); 
+
+console.log("Writing abi for contracts")
+for (var contractName in output.contracts['contracts/Terms.sol']) {
+    var contractAbi = JSON.stringify(output.contracts['contracts/Terms.sol'][contractName].abi, null, 2)
+    fs.writeFile('abi/' + contractName + ".abi", contractAbi, err => {
+        if (err) {
+          console.error(err)
+          return
+        }
+        //file written successfully
+      })
+  }
